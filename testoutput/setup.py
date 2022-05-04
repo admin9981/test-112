f96179bd0a070ba453211dd64d30e9fd748263c9
admin9981 <100740935+admin9981@users.noreply.github.com>
2022-05-04 10:51:45 +0900

    Create setup.py

---
+++ testoutput/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name = 'me_a',
+  version='1.0.0',
+  description='oss-dev my calculator 0001',
+  author = 'junghwan',
+  author_email = 'wwat1313@gmail.com',
+  url = "https://github.com/admin9981/test-112/",
+  #download_url =
+  install_url=['pytset']
+  long_descripyion = 'oss development....',
+  long_descripyion_content_type = 'text/markdown',
+  classifiers=[
+    "programming Language::python::3",
+    "Operating System::OS Independent",
+  ],
+)
